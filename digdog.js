d946ec848f1c55588d15aa17e512070d7359f135
Maddie Jones <maddiejones@Maddies-MacBook-Air.local>
2021-03-03 09:50:40 -0600

    Initial commit

---
+++ digdog.js
@@ -0,0 +1,35 @@
+const NUMBONES = 5;
+for (int column = 0; column < NUMBONES; column++) {
+    for (int row = 0; row < NUMBONES; row++) {
+    }
+
+}
+$('.button').click(function (
+    $rand('.image').show();
+));
+function changeColor(color) {
+    document.getElementsByClassName('btn').backgroundColor;
+}
+function click() {
+    changeColor('brown');
+    button.disabled = true;
+}
+
+}
+function bones() {
+    var element = document.createElement('h1');
+    var i = [];
+    if (function click() = '#bone') {
+        print('Bones Found: ' + i--);
+    } else {
+
+    }
+}
+function bar() {
+    var barmove = document.getElementById("dangerbar");
+    var width = math.random(.005 - .10);
+    if (function click() = true) {
+        width++;
+    };
+
+}
